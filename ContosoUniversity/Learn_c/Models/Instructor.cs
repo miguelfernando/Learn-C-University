eadb5701e8123e016ba8961ba8701f08e40aa280
miguelfernando <miguelfernando@globo.com>
2020-03-11 17:37:43 -0300

    Atualizar dados relacionados

---
+++ ContosoUniversity/Learn_c/Models/Instructor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Instructor
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Last Name"), StringLength(50, MinimumLength = 1)]
+        public string LastName { get; set; }
+
+        [Column("FirstName"), Display(Name = "First Name"), StringLength(50, MinimumLength = 1)]
+        public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date), Display(Name = "Hire Date"), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime HireDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+
+        private ICollection<Course> _courses;
+        public virtual ICollection<Course> Courses
+        {
+            get
+            {
+                return _courses ?? (_courses = new List<Course>());
+            }
+            set
+            {
+                _courses = value;
+            }
+        }
+
+        public virtual OfficeAssignment OfficeAssignment { get; set; }
+    }
+}
